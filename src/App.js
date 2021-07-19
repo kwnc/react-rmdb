5e6d39211cf187ee5f58501391412f233fb7e712
Konrad Wenc <imkonradwenc@gmail.com>
2021-07-19 20:19:42 +0200

    Hero Image fetched

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
 // Components
 import Header from './components/header';
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <GlobalStyle/>
     </div>
   );
